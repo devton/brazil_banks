a974a386580775d961a6f250f5484f835ca0e1ac
Antônio Roberto Silva <forevertonny@gmail.com>
2014-08-18 23:28:36 -0300

    replace \n\t and trim bank name

--- app/scrapper.py
+++ app/scrapper.py
@@ -1,3 +1,4 @@
+import re
 import json
 from bs4 import BeautifulSoup
 
@@ -6,6 +7,7 @@ class Scrapper(object):
     def __init__(self, html_text):
         self.soup = BeautifulSoup(html_text)
         self.banks = []
+        self.regex = re.compile(r'[\n\r\t]')
         self.__get_bank_table()
         self.__get_banks()
 
@@ -21,8 +23,8 @@ class Scrapper(object):
         for index in range(2, len(rows)):
             try:
                 cols = rows[index].find_all('td')
-                bank_uid = cols[0].text
-                bank_name = cols[1].find('a').text
+                bank_uid = self.regex.sub('', cols[0].text).strip()
+                bank_name = self.regex.sub('', cols[1].find('a').text).strip()
                 self.banks.append({'id': bank_uid, 'name': bank_name})
             except:
                 pass
